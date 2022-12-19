ca6ebfd57a95b7c922e64261eac8ed9ee1e5f6c7
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2022-12-19 14:24:07 +0100

    project complete
    u can know register user and login and logout and user can create post

--- clean-blog/server/index.js
+++ clean-blog/server/index.js
@@ -43,6 +43,7 @@ app.use('*',(req,res,next)=>{
 app.get('/',async (req,res)=>{
   
     const blogposts= await BlogPost.find({}).populate('userid')
+    console.log(blogposts)
     console.log('Calling  / home')
     res.render('index',{
     
