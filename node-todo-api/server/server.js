3bd052ba0da13ade03a8fc5162ea9a7839e74fa2
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2023-07-19 12:40:46 +0100

    Add GET /todos/:id route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -3,7 +3,7 @@ const { Todo } = require("./db/models/todo");
 const { User } = require("./db/models/user");
 const express = require("express");
 const body_parser = require("body-parser");
-
+const {ObjectID} = require('mongodb');
 const app = express();
 app.use(body_parser.json());
 
@@ -34,4 +34,21 @@ app.get("/todos", async (req, res) => {
     (e) => res.status(400).send(e)
   );
 });
+//GET by Id
+app.get('/todos/:id',(req,res)=>{
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+     return   res.status(400).send("Id not Valid")
+    }
+    Todo.findById(req.params.id).then((todo)=>{
+        if(todo){
+            res.send({todo})
+        }else {
+            
+            res.status(404).send({
+                todo : `No todo found for this id ${req.params.id}`
+            })
+        }
+    }, (e) => res.status(400).send(e))
+
+})
 module.exports = { app };
