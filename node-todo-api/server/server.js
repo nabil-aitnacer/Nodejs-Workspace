dec1ad9949a476187912a8788de7d39134941fe9
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2023-07-19 12:04:16 +0100

    Add GET /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,27 +1,37 @@
-const {mongoose} = require('./db/mongoose')
-const {Todo} = require('./db/models/todo')
-const {User} = require('./db/models/user')
-const express = require('express')
-const body_parser = require('body-parser')
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./db/models/todo");
+const { User } = require("./db/models/user");
+const express = require("express");
+const body_parser = require("body-parser");
 
-const app = express()
-app.use(body_parser.json())
+const app = express();
+app.use(body_parser.json());
 
-app.listen(3000,()=>{
-    console.log("Server Lesteninng on port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server Lesteninng on port 3000");
+});
 
-app.post('/todos',(req,res)=>{
-    const newTodo = new Todo({
-        text:req.body.text,
-        completed:req.body.completed,
-        completedAt:req.body.completedAt
-    })
-    newTodo.save().then((doc)=>{
-        res.send(doc)
-    },(e)=>{
-        res.status(400).send(e)
-    })
-})
-
-module.exports = {app}
+app.post("/todos", (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text,
+    completed: req.body.completed,
+    completedAt: req.body.completedAt,
+  });
+  newTodo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
+});
+app.get("/todos", async (req, res) => {
+  Todo.find({}).then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => res.status(400).send(e)
+  );
+});
+module.exports = { app };
