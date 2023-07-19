5fe63e52d8aa5827bd117f8233f77dae6da9bbff
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2023-07-19 11:48:59 +0100

    Test POST /todos route

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -1,17 +1,58 @@
-const { app } = require("../server");
-const { Todo } = require("../db/models/todo");
+const expect = require('expect');
+const request = require('supertest');
 
-const request = require("supertest");
-const expect = require("expect");
+const {app} = require('./../server');
+const {Todo} = require('./../db/models/todo');
 
-//use describe to group all of the routes
+const todos = [{
+   text: 'First test todo'
+},{
+   text: 'Second test todo'
+}];
 
-describe("POST /todos", () => {
-  it("it should create new todo", (done) => {
-    var text = " text todo test";
+beforeEach((done) =>{
+  Todo.deleteMany({}).then(() => {
+    
+  }).then(() => done());
+});
 
+describe('POST /todos', () => {
+   it('should create a new todo',(done) => {
+     var text = 'Test todo text';
+     request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+          expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if(err) {
+         return done(err);
+        }
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+   })
+
+   it('should not create todo with invalid body data', (done) => {
     request(app)
     .post('/todos')
-    .send({text})
+    .send({})
+    .expect(400)
+    .end((err, res) => {
+      if(err){
+        return done(err);
+      }
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 });
+
+
