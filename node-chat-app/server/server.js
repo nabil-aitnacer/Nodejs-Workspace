6834d3fe1cd780989626c3b3a7d1a00b420a3e37
aitnacer-nabil <118471449+aitnacer-nabil@users.noreply.github.com>
2023-07-21 12:57:15 +0100

    Add onConenct and siconnect function handler socket.io

---
+++ node-chat-app/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname,'../','public')))
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
